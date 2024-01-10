ad79d29a752429f917f3531fbfac6e47e484b570
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-10 22:07:07 +0530

    Folder Structure Assembly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+
+import Home from './pages/home/Home';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+import Order from './pages/order/Order';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/*" element={<NoPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
