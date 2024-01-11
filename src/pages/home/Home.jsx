b262950e7c15d92163f89fb5539cec641e9f84f9
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-11 23:41:04 +0530

    Edited Home Page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
-import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/HeroSection'
 
 function Home() {
   
   return (
    <Layout>
-   Home
+    <HeroSection/>
    </Layout>
   )
 }
