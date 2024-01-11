3637c624715154fd815cb1e65633dc450447e5d5
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-11 22:13:36 +0530

    Created NavBar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,10 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 
 function Home() {
-  const context = useContext(myContext);
-  const {state, color} = context; 
+  
   return (
    <Layout>
-    <h1>Name: {state.name}</h1>
-    <h1>Roll No. : {state.rollno}</h1>
-    <h1>My Color: {color}</h1>
+   Home
    </Layout>
   )
 }
