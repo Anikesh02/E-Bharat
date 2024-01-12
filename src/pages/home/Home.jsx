1aa2a8de4b0b9681b39fa5fb8506bbc0601abda7
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-12 12:10:48 +0530

    Added Filter and Product Cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Track from '../../components/track/Track'
+import Testimonial from '../../components/testimonial/Testimonial'
 
 function Home() {
-  
+
   return (
-   <Layout>
-    <HeroSection/>
-   </Layout>
+    <Layout>
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
+    </Layout>
   )
 }
 
