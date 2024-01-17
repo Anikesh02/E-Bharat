d7b4f724e12e7d356a2000f761e13b1f3dad5d5f
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-17 21:42:39 +0530

    Firebase Setup

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpzfEPiq9RiHiMXiFYv0ELNpBreRlai4A",
+  authDomain: "e-bharat-c0a2d.firebaseapp.com",
+  projectId: "e-bharat-c0a2d",
+  storageBucket: "e-bharat-c0a2d.appspot.com",
+  messagingSenderId: "408633071827",
+  appId: "1:408633071827:web:e09fbce427880aa77edc26"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+export {fireDB,auth } ;
