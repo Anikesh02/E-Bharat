5f342468c0a686bdfc6a0ff0a5375db82f3fa148
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-01-22 00:11:00 +0530

    Added Separate Routing System for Users and Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import Home from './pages/home/Home';
-import Cart from './pages/cart/Cart';
-import Dashboard from './pages/admin/dashboard/Dashboard';
-import NoPage from './pages/nopage/NoPage';
-import Order from './pages/order/Order';
-import MyState from './context/data/myState';
-import Signup from './pages/registration/Signup';
-import Login from './pages/registration/Login';
-import ProductInfo from './pages/productInfo/ProductInfo';
-import UpdateProduct from './pages/admin/page/UpdateProduct';
-import AddProduct from './pages/admin/page/addProduct';
-import { ToastContainer} from 'react-toastify';
+import { BrowserRouter as Router, Route, Routes, Navigate, } from "react-router-dom";
+import Home from "./pages/home/Home";
+import MyState from "./context/data/myState";
+import Order from "./pages/order/Order";
+import NoPage from "./pages/nopage/NoPage";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
+import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import AddProduct from "./pages/admin/page/AddProduct";
+import UpdateProduct from "./pages/admin/page/UpdateProduct";
 
 function App() {
   return (
@@ -21,20 +19,43 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/order" element={<Order />} />
+          <Route path="/order" element={<ProtectedRoutes><Order /></ProtectedRoutes>} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<ProtectedRoutesForAdmin><Dashboard /></ProtectedRoutesForAdmin>} />
+          <Route path="/productinfo" element={<ProductInfo />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/addProduct" element={<AddProduct />} />
-          <Route path="/updateProduct" element={<UpdateProduct />} />
-          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/addproduct" element={<ProtectedRoutesForAdmin><AddProduct /></ProtectedRoutesForAdmin>} />
+          <Route path="/updateproduct" element={<ProtectedRoutesForAdmin><UpdateProduct /></ProtectedRoutesForAdmin>} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
         <ToastContainer />
       </Router>
     </MyState>
+
   )
 }
 
 export default App
+
+//User can't go to order page without login
+export const ProtectedRoutes = ({ children }) => {
+  if (localStorage.getItem('user')) {
+    return children
+  }
+  else {
+    return <Navigate to='/login' />
+  }
+}
+
+//Admin can't go to dashboard page without login
+export const ProtectedRoutesForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem('user'))
+  console.log(admin.user.email)
+  if (admin.user.email === 'anikeshkulal@gmail.com') {
+    return children
+  }
+  else {
+    return <Navigate to='/login' />
+  }
+}
